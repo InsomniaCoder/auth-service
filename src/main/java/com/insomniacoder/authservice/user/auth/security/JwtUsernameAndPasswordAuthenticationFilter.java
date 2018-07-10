a05637668a5f01d0434aff21e99e77fdb82862ac
InsomniaCoder <tanatloke@gmail.com>
2018-07-10 13:41:35 +0700

    add security and jwt generator

---
+++ src/main/java/com/insomniacoder/authservice/user/auth/security/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -0,0 +1,96 @@
+package com.insomniacoder.authservice.user.auth.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Date;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+
+public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    // We use auth manager to validate the user credentials
+    private AuthenticationManager authManager;
+
+    private final String AUTH_URL = "/auth/**";
+    private final String AUTHORIZATION_HEADER = "Authorization";
+    private final String BEARER_PREFIX = "Bearer ";
+    private final String _secret = "JwtSecretKey";
+
+    private final int EXPIRATION_PERIOD = 24 * 60 * 60;
+
+    public JwtUsernameAndPasswordAuthenticationFilter(AuthenticationManager authManager) {
+
+        this.authManager = authManager;
+        // By default, UsernamePasswordAuthenticationFilter listens to "/login" path.
+        // In our case, we use "/auth". So, we need to override the defaults.
+        this.setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher(this.AUTH_URL, "POST"));
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+
+        try {
+            // 1. Get credentials from request
+            UserCredentials userCredentials = new ObjectMapper().readValue(request.getInputStream(), UserCredentials.class);
+            // 2. Create auth object (contains credentials) which will be used by auth manager
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                    userCredentials.getUsername(), userCredentials.getPassword(), Collections.emptyList());
+            // 3. Authentication manager authenticate the user, and use UserDetialsServiceImpl::loadUserByUsername() method to load the user.
+            return authManager.authenticate(authToken);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // Upon successful authentication, generate a token.
+    // The 'auth' passed to successfulAuthentication() is the current authenticated user.
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+                                            Authentication auth) throws IOException, ServletException {
+
+        Long now = System.currentTimeMillis();
+
+        String token = Jwts.builder()
+                .setSubject(auth.getName())
+                // Convert to list of strings.
+                // This is important because it affects the way we get them back in the Gateway.
+                .claim("authorities", auth.getAuthorities().stream()
+                        .map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                .setIssuedAt(new Date(now))
+                .setExpiration(new Date(now + EXPIRATION_PERIOD * 1000))  // in milliseconds
+                .signWith(SignatureAlgorithm.HS512, _secret.getBytes())
+                .compact();
+        // Add token to header
+        response.addHeader(AUTHORIZATION_HEADER, BEARER_PREFIX + token);
+    }
+
+    // A (temporary) class just to represent the user credentials
+    private static class UserCredentials {
+        private String username, password;
+
+        public String getUsername() {
+            return this.username;
+        }
+
+        public String getPassword() {
+            return this.password;
+        }
+    }
+}
---
+++ src/main/java/com/insomniacoder/authservice/user/auth/security/UserDetailsServiceImpl.java
@@ -0,0 +1,41 @@
+package com.insomniacoder.authservice.user.auth.security;
+
+import com.insomniacoder.authservice.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        com.insomniacoder.authservice.user.entity.User appUser = userRepository.findByEmail(username);
+
+        if (appUser != null) {
+
+            List<GrantedAuthority> grantedAuthorities = AuthorityUtils
+                    .commaSeparatedStringToAuthorityList("ROLE_" + appUser.getRole());
+            return new User(
+                    appUser.getEmail(),
+                    encoder.encode(appUser.getPassword()), grantedAuthorities);
+        }
+        throw new UsernameNotFoundException("Username: " + username + " not found");
+    }
+
+}
